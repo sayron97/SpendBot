fef194e83150c777509b80a6aeaf51bf8eb4eb4a
Alex <sashaef02@gmail.com>
2020-09-05 01:04:12 +0300

    add langs

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const new_product_button = require("./migrations/blocks").newProductButton();
 const block_after_new_spend = require("./migrations/blocks").blockAfterNewSpend();
 
 const tesseract_config = {
-    lang: "eng",
+    lang: "rus+ukr",
     oem: 1,
     psm: 3,
 }
